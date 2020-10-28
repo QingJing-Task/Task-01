9862edfd59abd99eb36abe752b0d48de6b73a37e
lenovo <CnMrLi@163.com>
2020-10-28 10:35:05 +0800

    创建分支

--- src/main/java/cn/aiweiyi/qingjing/task/TaskDemo.java
+++ src/main/java/cn/aiweiyi/qingjing/task/TaskDemo.java
@@ -4,5 +4,6 @@ public class TaskDemo {
     public static void main(String[] args) {
         System.out.println("哈哈哈");
         System.out.println("笑死啦");
+        System.out.println("创建分支!");
     }
 }
