98eaa45696e226e2a2b2a8dbff3a3b00fb074064
lenovo <CnMrLi@163.com>
2020-10-28 10:24:27 +0800

    添加新事物

--- src/main/java/cn/aiweiyi/qingjing/task/TaskDemo.java
+++ src/main/java/cn/aiweiyi/qingjing/task/TaskDemo.java
@@ -3,5 +3,6 @@ package cn.aiweiyi.qingjing.task;
 public class TaskDemo {
     public static void main(String[] args) {
         System.out.println("哈哈哈");
+        System.out.println("笑死啦");
     }
 }
